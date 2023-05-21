d47f4cda95c04b47e6962957d31398e528ea94fc
wildersonh <wildersonhernandez994@gmail.com>
2023-05-21 14:18:27 -0430

    Hello github

--- hellogit.py
+++ hellogit.py
@@ -1,4 +1,1 @@
-print("new  Hello git with changes!")
-salir = input("quieres salir: ")
-if(salir == "s"):
-	print("chao")
+print("new  Hello github!")
--- hellogit3.py
+++ hellogit3.py
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 print("Hello git 3 v loginh")
+=======
+print("Hello git 3 v3")
+>>>>>>> 962e4ee (g 3)
